fac5c9a095f6a8c7eb49d2a1a500632f5ee304c1
benalun-jones <benalunjones@gmail.com>
2015-12-12 17:29:15 +0000

    app.js functioning with no errors

--- api/app.js
+++ api/app.js
@@ -1,45 +1,24 @@
-var express = require('express');
-var morgan = require('morgan');
+var express    = require('express');
+var morgan     = require('morgan');
 var bodyParser = require('body-parser');
-var mongoose       = require('mongoose');
-var path = require('path');
-
-var app = express();
-// var passport       = require('passport');
+var mongoose   = require('mongoose');
+var path       = require('path');
+var cors       = require('cors');
 var methodOverride = require("method-override");
-// var MongooseVoting = require('mongoose-voting');
-// var jwt            = require('jsonwebtoken');
-// var expressJWT     = require('express-jwt');
+var app = express();
 
 mongoose.connect('mongodb://localhost:27017/mindmerge');
 
-// app.use(methodOverride(function(req, res){
-//   if (req.body && typeof req.body === 'object' && '_method' in req.body) {
-//     var method = req.body._method
-//     delete req.body._method
-//     return method
-//   }
-// }));
-
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
-app.use(morgan('dev'));
-
-
-// var config         = require('./config/routes');
-// var User           = require('./models/user');
-// var Group          = require('./models/group');
-// var Task       = require('./models/task');
-
 var routes = require('./config/routes');
-app.use("/api", routes);
+
+// app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cors());
+app.use(routes);
 
 app.get('/', function(req, res){
   res.send('hello world from express');
-});
-
-app.post('/users', function(req, res) {
-
 });
 
 app.listen(3000);
--- api/config/routes.js
+++ api/config/routes.js
@@ -3,19 +3,7 @@ var router = express.Router();
 var bodyParser = require('body-parser');
 var methodOverride = require('method-override');
 
-var authenticationController = require('../controllers/authenticationController');
-var agentsController = require('../controllers/agentsController');
+var userController = require('../controllers/users_controller');
 
-router.post('/authorize', authenticationController.login);
-router.post('/join', authenticationController.signup);
-
-router.route('/agents')
-  .get(agentsController.indexAgents)
-  .post(agentsController.createAgent)
-
-router.route('/agents/:id')
-  .get(agentsController.showAgent)
-  .put(agentsController.updateAgent)
-  .delete(agentsController.deleteAgent)
 
 module.exports = router;
