5bcb99c7f88c257ece0582a770f1a84b6f40a6de
benalun-jones <benalunjones@gmail.com>
2015-12-12 12:18:12 +0000

    added error message for no access token fb authentication

--- api/controllers/authentications _controller.js
+++ api/controllers/authentications _controller.js
@@ -2,3 +2,7 @@ var User = require('../models/user');
 var request = require('request');
 var jwt = require('jsonwebtoken');
 // var secret = process.env.PLANNERR_JWT_SECRET;
+
+
+function facebook(req, res) {
+  if(!req.body.access_token) return res.status(401).json({ message: 'No Access Token' });
