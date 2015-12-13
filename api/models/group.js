7e095779833ee22bf89c2954057da35b3946bcf7
benalun-jones <benalunjones@gmail.com>
2015-12-13 13:22:26 +0000

    added task and group model

--- api/models/group.js
+++ api/models/group.js
@@ -1,30 +1,10 @@
-// var mongoose = require("mongoose");
-// var Task   = require('./task');
+var mongoose = require('mongoose');
 
-// var groupSchema = new mongoose.Schema({
-//   name: String,
-//   activity_duration: String,
-//   deadline: Date,
-//   image: String,
-//   admin_user: {type: mongoose.Schema.ObjectId, ref: 'User'},
-//   tasks: [Task.schema],
-//   users: [{type: mongoose.Schema.ObjectId, ref: 'User'}]
-// });
+var GroupSchema = mongoose.Schema({
+  title: String,
+  users: [{type: mongoose.Schema.ObjectId, ref: 'User'}],
+  admin_user: {type: mongoose.Schema.ObjectId, ref: 'User'},
+  tasks: [Task.schema]
+})
 
-// // groupSchema.pre('save', function(next) {
-// //   var group = this;
-
-// //   // if admin_user is changed, check new admin user is in users array
-// //   if(group.isModified('admin_user') && group.users.indexOf(group.admin_user) === -1) {
-// //     return next(new Error('Admin User not present in users'));
-// //   }
-// //   // if users is changes, check admin has not been removed
-// //   console.log(group.users, group.admin_user);
-// //   if(group.isModified('users') && group.users.indexOf(group.admin_user) === -1) {
-// //     return next(new Error('Admin User has been removed from users'));
-// //   }
-
-// //   next();
-// // });
-
-// module.exports = mongoose.model("Group", groupSchema);
+module.exports = mongoose.model('Group', GroupSchema);
--- api/models/task.js
+++ api/models/task.js
@@ -1,1 +1,12 @@
-// var mongoose = require("mongoose");
+var mongoose = require("mongoose");
+
+
+var TaskSchema = new mongoose.Schema({
+  task_name: String,
+  author: String,
+  pages: String,
+  users_voted: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User' }],
+  assigned_user: {type: mongoose.Schema.ObjectId, ref: 'User'}
+});
+
+module.exports = mongoose.model('Task', TaskSchema);
