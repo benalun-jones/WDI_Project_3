6f0d34f5a9c1c3c628e98e71ca528fd94f24c323
benalun-jones <benalunjones@gmail.com>
2015-12-13 13:51:05 +0000

    added get, post and delete routes scaffold

--- api/config/routes.js
+++ api/config/routes.js
@@ -5,5 +5,16 @@ var methodOverride = require('method-override');
 
 var userController = require('../controllers/users_controller');
 
+app.get('/api/users', function(req, res) {
+
+  });
+
+app.post('/api/users', function(req, res) {
+
+  });
+
+app.delete('/api/users/:user_id', function(req, res) {
+
+  });
 
 module.exports = router;
