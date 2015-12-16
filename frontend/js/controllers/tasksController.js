4c926505ccf7a174f4c0bf5ecafc13bf20a5ccfd
benalun-jones <benalunjones@gmail.com>
2015-12-16 09:36:38 +0000

    added selectTask function

--- frontend/js/controllers/tasksController.js
+++ frontend/js/controllers/tasksController.js
@@ -1,12 +1,16 @@
 angular
-  .module('MindMergeApp')
-  .controller('TasksController', TasksController);
+.module('MindMergeApp')
+.controller('TasksController', TasksController);
 
-  TasksController.$inject = ['Task', '$resource'];
-  function GroupsController(Group, $resource) {
+TasksController.$inject = ['Task', '$resource'];
+function GroupsController(Group, $resource) {
 
-    var self = this;
-    this.task = {}
-    this.tasks = Task.query();
+  var self = this;
+  this.task = {}
+  this.tasks = Task.query();
 
-  }
+  this.selectTask = function(task) {
+    self.selectedTask = Task.get({ id: task._id });
+  };
+
+}
