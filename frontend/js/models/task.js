43af459440f93df316e38d1a7b9f97b0e2541802
benalun-jones <benalunjones@gmail.com>
2015-12-16 09:45:34 +0000

    added query, update, create and save methods in the task model for ng-resource

--- frontend/js/models/task.js
+++ frontend/js/models/task.js
@@ -4,13 +4,16 @@ angular
 
   Task.$inject = ['$resource', 'API_URL'];
 
-  function Task($resource){
-    var Task = $resource('http://localhost:3000/projects/:id', null, {
-      'update': { method:'PATCH' },
-      'create': { method: 'post' }
+  function Task($resource, API_URL){
+    return $resource(API_URL+'/tasks/:id', null, {
+      'query': {method: 'GET', params: {}, isArray: true },
+      'update': { method:'PUT' },
+      'create': { method: 'POST' },
+      'save': {method: 'POST' }
     });
-
     return Task;
 
 
   }
+
+
